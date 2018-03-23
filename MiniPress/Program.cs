a8997ca738efb0bd356db9e8761f1f0f84816984
MD.Saiful Ebne Hashem <saifulebnehashem@gmail.com>
2018-03-23 19:36:00 +0600

    Error
    Console.Write(Error) (html Code!!)

--- MiniPress/Program.cs
+++ MiniPress/Program.cs
@@ -20,7 +20,8 @@ namespace MiniPress
             string title = Console.ReadLine();
             Console.WriteLine("<!DOCTYPE html><html><head><title>{0}</title></head><body><h1>{1}</h1><p>{2}</p></body></html>",title,name,discription);
             */
-
+            Console.Write("<!--\nThis is Copyright Free Web Template For Everyone\n.................................................\nThis Webpage Create for MiniPress use!!\n- - - - - - - - - - - - - - - - - - - - - - - - -\nThis WebPage Make by MD.Saiful Ebne Hahshem\nFB: SaifulEbneHahshem\nWebsite: https://www.SaifulEbneHahshem.ml\nE - mail:me@saifulebnehahshem.ml\nPhone: +880 1633 700075\n..................................................\n-->\n<!DOCTYPE html>\n< html >\n< head >\n< title >{ 0}</ title >\n<link rel="stylesheet" type="text/css" href="./css/materialize.min.css">\n< script src = "./js/materialize.min.js"></script>\n</head>\n<body>\n<div class="z -depth-5 container #6a1b9a purple darken-3">\n<div class="center-align #4a148c purple darken-4 z-depth-2">\n< img class="responsive-img z-depth-5 profileimg" src="{1}">\n</div>\n< div class="center-align #8e24aa purple darken-1 z-depth-5 z-depth-5 hadingd1name">\n< h1 class="center-align hadingname">{2}</h1>\n</div>\n<div class="#aa00ff purple accent-4 z-depth-5">\n< table class="striped">\n<tbody>\n<tr>\n< td class="td">Name:</td>\n< td class="td2">&nbsp;&nbsp;&nbsp;{3}</td>\n</tr>\n<tr>\n< td class="td">Phone:</td>\n< td class="td2">&nbsp;&nbsp;&nbsp;{4}</td>\n</tr>\n<tr>\n<td class="td">Age:</td>\n< td class="td2">&nbsp;&nbsp;&nbsp;{5}</td>\n</tr>\n<tr>\n< td class="td">Email:</td>\n< td class="td2">&nbsp;&nbsp;&nbsp;{6}</td>\n</tr>\n<tr>\n< td class="td">Address:</td>\n< td class="td2">&nbsp;&nbsp;&nbsp;{7}</td>\n</tr>\n</tbody>\n</table>\n</div>\n</div>\n</body>\n</html>");
+            /*
             Console.Write("Input Your Full Name:");
             string name = Console.ReadLine();
 
@@ -42,8 +43,7 @@ namespace MiniPress
             Console.WriteLine("Input Your Web-Title:");
             string title = Console.ReadLine();
 
-
-
+            */
 
             /*
             Console.WriteLine("Input Gender:");
